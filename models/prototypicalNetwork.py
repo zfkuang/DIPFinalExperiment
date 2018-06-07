4c4e6d20bd142cbcaba64ac22ab08e6e28a95810
StopProgrammerAbusing <1054133189@qq.com>
2018-06-07 20:59:20 +0800

    added prototypical network, still need improve

--- models/prototypicalNetwork.py
+++ models/prototypicalNetwork.py
@@ -15,9 +15,12 @@ def conv_block(inputs, out_channels, name='conv'):
         conv = tf.contrib.layers.max_pool2d(conv, 2)
         return conv
 
-def encoder(x, output_dim, reuse=False):
+def encoder(x, output_dim, keep_prob, reuse=False):
     with tf.variable_scope('encoder', reuse=reuse):
-        net = tf.layers.dense(x, 300, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=0.1))
+        net = tf.layers.dense(x, 4096, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=0.1))
+        net = tf.nn.dropout(net, keep_prob)
+        net = tf.layers.dense(net, output_dim, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=0.1))
+        net = tf.nn.dropout(net, keep_prob)
         net = tf.layers.dense(net, output_dim, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=0.1))
         #net = tf.layer.AlexNet(x, 0.5, 2000, ["fc8"])
         #net = conv_block(x, h_dim, name='conv_1')
@@ -40,12 +43,12 @@ n_epochs = 100
 n_episodes = 100
 n_way = 50
 n_shot = 5
-n_query = 25
+n_query = 15
 im_width, im_height, channels = 227, 227, 3
 n_features = 4096
 h_dim = 32
 z_dim = 32
-output_dim = 50
+output_dim = 2048
 n_test_episodes = 100
 n_test_way = 50
 n_test_example = 10
@@ -68,23 +71,24 @@ def prototypicalNetwork(sess, trainData, trainLabel, trainIndex, testData, testL
 
     x = tf.placeholder(tf.float32, [None, None, n_features]) # num_classes, num_support, feature
     q = tf.placeholder(tf.float32, [None, n_features]) # num_queries, feature
+    keep_prob = tf.placeholder(tf.float32)
     x_shape = tf.shape(x)
     q_shape = tf.shape(q)
     num_classes, num_support = x_shape[0], x_shape[1]
     num_queries = q_shape[0]
     y = tf.placeholder(tf.int64, [None]) # num_queries
     y_one_hot = tf.one_hot(y, depth=num_classes)
-    emb_x = encoder(tf.reshape(x, [num_classes * num_support, n_features]), output_dim)
+    emb_x = encoder(tf.reshape(x, [num_classes * num_support, n_features]), output_dim, keep_prob)
     emb_dim = tf.shape(emb_x)[-1]
     emb_x = tf.reduce_mean(tf.reshape(emb_x, [num_classes, num_support, emb_dim]), axis=1)
-    emb_q = encoder(q, output_dim, reuse=True)
+    emb_q = encoder(q, output_dim, keep_prob, reuse=True)
     dists = euclidean_distance(emb_q, emb_x)
     log_p_y = tf.reshape(tf.nn.log_softmax(-dists), [num_queries, -1])
     ce_loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_one_hot, log_p_y), axis=-1), [-1]))
     logits = tf.argmax(log_p_y, axis=-1) 
     acc = tf.reduce_mean(tf.to_float(tf.equal(logits, y)))
 
-    params = tf.trainable_variables()[-4:]
+    params = tf.trainable_variables()[-6:]
     print(params)
     train_op = tf.train.AdamOptimizer().minimize(ce_loss, var_list=params)
 
@@ -106,7 +110,7 @@ def prototypicalNetwork(sess, trainData, trainLabel, trainIndex, testData, testL
             # labels in training doesn't matter at all
             labels = np.tile(np.arange(n_way)[:, np.newaxis], (1, n_query)).astype(np.uint8)
             labels = labels.reshape([n_way*n_query])
-            _, ls, ac, logy = sess.run([train_op, ce_loss, acc, log_p_y], feed_dict={x: support, q: query, y:labels})
+            _, ls, ac, logy = sess.run([train_op, ce_loss, acc, log_p_y], feed_dict={x: support, q: query, y:labels, keep_prob: 0.7})
             if (epi+1) % 50 == 0:
                 print(logy[:5])
                 print('[epoch {}/{}, episode {}/{}] => loss: {:.5f}, acc: {:.5f}'.format(ep+1, n_epochs, epi+1, n_episodes, ls, ac))
@@ -127,7 +131,7 @@ def prototypicalNetwork(sess, trainData, trainLabel, trainIndex, testData, testL
             query = query.reshape([n_test_way*n_test_query, n_features])
             labels = np.tile(np.arange(n_test_way)[:, np.newaxis], (1, n_test_query)).astype(np.uint8)
             labels = labels.reshape([n_test_way*n_test_query])
-            ac, ls, logit, logy = sess.run([acc, ce_loss, logits, log_p_y], feed_dict={x: support, q: query, y:labels})
+            ac, ls, logit, logy = sess.run([acc, ce_loss, logits, log_p_y], feed_dict={x: support, q: query, y:labels, keep_prob: 1.0})
             if epi == 0:
                 print(logy[:5])
                 print(logit[:5])
